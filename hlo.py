93d017548e981f95867d9f82ad8434b8f1bfdda2
VAIBHAV CHAUHAN <123004674+UDAAKADAL@users.noreply.github.com>
2024-01-04 08:56:15 +0530

    Create hlo.py

---
+++ hlo.py
@@ -0,0 +1,41 @@
+# a=input("enter your number :")
+# print("your name is :",a)
+# x= int (input("enter first number :"))
+# y=int (input("enter second number :"))
+# print("addition :" ,x + y)
+# print("subtraction :",x - y)
+# print("divide :" ,x / y)
+# print("multiplication :",x * y)
+# print("reminder :" ,x % y)
+
+
+#day 11
+# andry = '''vaibhav'''
+# for character in andry:
+#  print(character)
+ 
+# #day 12
+# names = "ankit ka bacchi"
+# print(names[0:9])
+# print(len(names))
+# print(names[0:15:2])
+# print(names[:])  
+# print(names[2:10]) 
+# print(names[3:-1]) 
+# print(names[-5:-2]) 
+# print(names[0:-6]) 
+
+# # day 13
+# # strint are unmutable
+# str="Vaibhav"
+# print (str.upper())
+# print(str.lower())
+# print(str.title())
+# print(str.rstrip('v'))
+# print(str.lstrip('V'))
+# print(str.replace('Vaibhav','ankit'))
+
+
+
+
+      
