9cb0fb251a58fc234c05a6ee645564b889fe92c1
VAIBHAV CHAUHAN <123004674+UDAAKADAL@users.noreply.github.com>
2024-01-04 17:02:41 +0530

    100 days of python

--- hlo.py
+++ hlo.py
@@ -1,39 +1,72 @@
-# a=input("enter your number :")
-# print("your name is :",a)
-# x= int (input("enter first number :"))
-# y=int (input("enter second number :"))
-# print("addition :" ,x + y)
-# print("subtraction :",x - y)
-# print("divide :" ,x / y)
-# print("multiplication :",x * y)
-# print("reminder :" ,x % y)
+a=input("enter your number :")
+print("your name is :",a)
+x= int (input("enter first number :"))
+y=int (input("enter second number :"))
+print("addition :" ,x + y)
+print("subtraction :",x - y)
+print("divide :" ,x / y)
+print("multiplication :",x * y)
+print("reminder :" ,x % y)
 
 
-#day 11
-# andry = '''vaibhav'''
-# for character in andry:
-#  print(character)
+day 11
+andry = '''vaibhav'''
+for character in andry:
+ print(character)
  
-# #day 12
-# names = "ankit ka bacchi"
-# print(names[0:9])
-# print(len(names))
-# print(names[0:15:2])
-# print(names[:])  
-# print(names[2:10]) 
-# print(names[3:-1]) 
-# print(names[-5:-2]) 
-# print(names[0:-6]) 
+#day 12
+names = "ankit ka bacchi"
+print(names[0:9])
+print(len(names))
+print(names[0:15:2])
+print(names[:])  
+print(names[2:10]) 
+print(names[3:-1]) 
+print(names[-5:-2]) 
+print(names[0:-6]) 
 
-# # day 13
-# # strint are unmutable
-# str="Vaibhav"
-# print (str.upper())
-# print(str.lower())
-# print(str.title())
-# print(str.rstrip('v'))
-# print(str.lstrip('V'))
-# print(str.replace('Vaibhav','ankit'))
+# day 13
+# string are unmutable
+str ="Vaibhav"
+print (str.upper())
+print(str.lower())
+print(str.rstrip('v'))
+print(str.lstrip('V'))
+print(str.replace('Vaibhav','khushi'))
+print(str.split("b"))
+str1="vaibhav chauhan"
+print(str1.capitalize())
+print(str1.title())
+print(len(str1))
+print(len(str1.center(50)))
+print(str1.count("vaibhav"))
+print(str.startswith("V"))
+print(str.endswith("v"))
+print(str.endswith("a",1,5))
+print(str.find("ai"))
+print(str.index("ai"))
+str = " hlo ji kya hal chal"
+str2= "     "
+print(str.isalnum())
+print(str.isalpha())
+print(str.islower())
+print(str.isupper())
+print(str.isprintable())
+print(str2.isspace())
+print(str.istitle())
+print(str.swapcase())
+
+#DAY 14
+#if ,else,elif
+a=int(input("apna age btao babua: "))
+if(a>=18 and a<60):
+    print("vote mujhe hi dena")
+elif(a>+60):
+    print("are dada ji aram karo jakar thodi si to zindagi bachi hai")    
+else:
+    print("jakar complaint piyo")    
+
+
 
 
 
