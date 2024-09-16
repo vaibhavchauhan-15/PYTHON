7d2247d7a0b9a7e584252440b8744933af923508
VAIBHAV CHAUHAN <123004674+UDAAKADAL@users.noreply.github.com>
2024-09-16 10:03:24 +0530

    Update hlo.py

--- hlo.py
+++ hlo.py
@@ -6,7 +6,7 @@ y=int (input("enter second number :"))
 print("addition :" ,x + y)           
 print("subtraction :",x - y)         
 print("divide :" ,x / y)         
-print("multiplication :",x * y)       
+print("multiplication :",x * y)        
 print("reminder :" ,x % y)            
  
 day 11              
