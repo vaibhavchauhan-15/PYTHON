18368c2530f1f6872023425d2d412f1f7dc26612
VAIBHAV CHAUHAN <123004674+vaibhavchauhan-15@users.noreply.github.com>
2025-03-28 23:11:30 +0530

    Update hlo.py

--- hlo.py
+++ hlo.py
@@ -6,7 +6,7 @@ x = int(input("Enter first number: "))  # Taking first number as input
 y = int(input("Enter second number: "))  # Taking second number as input  
  
 print("Addition:", x + y)  # Performing addition 
-print("Subtraction:", x - y)  # Performing subtraction
+print("Subtraction:", x - y)  # Performing subtraction 
 print("Division:", x / y)  # Performing division
 print("Multiplication:", x * y)  # Performing multiplication
 print("Remainder:", x % y)  # Calculating remainder
